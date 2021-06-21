ed22f7094cf0acfbdb03f7df0dee9bd0a92dcf90
Nathalia Dionizio <78830679+nathalia-dionizio@users.noreply.github.com>
2021-06-20 21:01:34 -0300

    Projeto 3 revisão JS

--- exercicios.js
+++ exercicios.js
@@ -1,118 +1,241 @@
-// Exemplos
-
-// Exercício 0A
-function soma() {
-  // escreva seu código aqui
-  const num1 = prompt('Digite o primeiro número')
-  const num2 = prompt('Digite o segundo número')
-
-  console.log(Number(num1) + Number(num2))
+// EXERCÍCIO 01
+function inverteArray(array) {
+  
 }
 
-// Exercício 0B
-function imprimeMensagem() {
-  // escreva seu código aqui
-  const mensagem = prompt('Digite sua mensagem')
 
-  console.log(mensagem)
+// EXERCÍCIO 02
+function retornaNumerosParesElevadosADois(array) {
+let numerosParesElevado = []
+for(let i = 0; i < array.length; i ++) {
+  if(array[i] % 2 === 0 ){
+    numerosParesElevado.push(array[i]**2)
+  }
+} return numerosParesElevado
 }
 
-// ---------------------------------------------------
-// Exercícios
-
-// Exercício 1
-function calculaAreaRetangulo() {
-  const num1 = prompt('Digite o primeiro número')
-  const num2 = prompt('Digite o segundo número')
-  console.log(Number(num1)*Number(num2))
-}
-// Exercício 2
-function imprimeIdade() {
- var anoAtual = prompt('Digite o ano atual')
- var dataNascimento = prompt('Digite sua data de nacimento')          
-console.log(anoAtual - dataNascimento)
-
+// EXERCÍCIO 03
+function retornaNumerosPares(array) {
+  let pares = []
+  for(let i = 0; i < array.length; i ++) {
+  if(array[i] % 2 === 0 ) {
+    pares.push(array[i])
+  }
+  } return pares
 }
 
-// Exercício 3
-function calculaIMC() {
-var peso = prompt('Qual o seu peso corporal?')
-var altura = prompt('Qual a sua altura?')
-console.log(peso / (altura * altura))
+// EXERCÍCIO 04
+function retornaMaiorNumero(array) {
+ let maiorNumero = []
+ for(let i = 0; i < array.length; i ++) {
+  if(array[i] > maiorNumero) {
+    maiorNumero = array[i]
+  }
+  } return maiorNumero
 }
 
-// Exercício 4
-function imprimeInformacoesUsuario() {
-const nome = prompt("Digite seu nome")
-const idade = Number(prompt("Digite sua idade"))
-const email = prompt("Digite seu e-mail")
-console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
+
+// EXERCÍCIO 05
+function retornaQuantidadeElementos(array) {
+  let quantElementos = 0
+ for(let i = 0; i < array.length; i ++) {
+  if(array[i] = array.length) {
+    quantElementos = array[i]
+  }
+  } return quantElementos
 }
 
-// Exercício 5
-function imprimeTresCoresFavoritas() {
-  const cor1 = prompt("Primeira cor")
-  const cor2 = prompt("Segunda cor")
-  const cor3 = prompt("Terceira cor")
-  const arrayCores = [cor1, cor2, cor3]
-  console.log(arrayCores)
+// EXERCÍCIO 06
+function retornaExpressoesBooleanas() {
+  const respostas = [false, false, true, true, true] 
+  return respostas 
+} 
+
+// EXERCÍCIO 07
+function retornaNNumerosPares(n) {
+  let numero = 3
+  let pares = []
+  for(let i = 0; i < n; i ++) {
+    pares.push(i*2)
+  } return pares
+} retornaNNumerosPares(numero)
+
+// EXERCÍCIO 08
+function checaTriangulo(a, b, c) {
+
+ if(a === b && b === c && c === a){
+  return 'Equilátero'
+ }else if(a !== b && b !== c && c !== a) {
+  return 'Escaleno'
+ } else{
+  return 'Isósceles'
+ }
 }
 
-// Exercício 6
-function retornaStringEmMaiuscula() {
-const palavra = prompt("Escreva uma palavra aleatoria")
-const palavraMaiuscula = palavra.toUpperCase()
-console.log(palavraMaiuscula)
+// EXERCÍCIO 09
+function comparaDoisNumeros(num1, num2) {
+ const comparacaoDeNumeros = {
+  maiorNumero: 15,
+  maiorDivisivelPorMenor: true,
+  diferenca: 0
+ }
+   if(num1 > num2) {
+  comparacaoDeNumeros.maiorNumero = num1
+  comparacaoDeNumeros.maiorDivisivelPorMenor = (num1 % num2 ===0)
+  comparacaoDeNumeros.diferenca = (num1 - num2)
+  }else if (num2 > num1) {
+  comparacaoDeNumeros.maiorNumero = num2
+  comparacaoDeNumeros.maiorDivisivelPorMenor = (num2 % num1 ===0)
+  comparacaoDeNumeros.diferenca = (num2 - num1)
+ 
+ } return comparacaoDeNumeros
+   
+ }
+
+// EXERCÍCIO 10
+function segundoMaiorEMenor(array) {
+  let arrayOriginal = [1 ,2 , 3, 4, 5, 6, 7]
+  let novoArray = []
+  
+  for(let i = 0; i < arrayOriginal.length; i ++) {  
+   if(array[i] > arrayOriginal) {
+     novoArray.push = array[i]
+   }else if(array[i] - 1 > arrayOriginal) {
+     novoArray.push = array[i]
+ 
+  }} return novoArray
+
+}
+       
+
+// EXERCÍCIO 11
+function ordenaArray(array) {
+
 }
 
-// Exercício 7
-function calculaIngressosEspetaculo() {
-const custoEspetaculo = prompt("Qual o custo do espetaculo?")
-const valorCadaIngresso = prompt("Qual o valor de cada ingresso?")
-const quantosIngressosPrecisaVender = custoEspetaculo / valorCadaIngresso
-console.log(quantosIngressosPrecisaVender)
-}
-3
-// Exercício 8
-function checaStringsMesmoTamanho() {
-const fruta = prompt("Digite nome de uma fruta")                        
-const carro = prompt("digite um nome de carro")
-const tamanho = fruta.length == carro.length
-console.log(tamanho)
+// EXERCÍCIO 12
+function filmeFavorito() {
+const filmeFavorito1 = {
+nome: "O Diabo Veste Prada",
+ano: 2006,
+diretor: "David Frankel",
+atores: ["Meryl Streep, Anne Hathaway, Emily Blunt, Stanley Tucci"]
+
+} 
+return(`nome: ${filmeFavorito1.nome}, ano: ${filmeFavorito1.ano}, diretor: ${filmeFavorito1.diretor}, atores: ${filmeFavorito1.atores}`)
+} 
+
+// EXERCÍCIO 13
+
+const filmeFavorito1 = {
+  nome: "O Diabo Veste Prada",
+  ano: 2006,
+  diretor: "David Frankel",
+  atores: ["Meryl Streep, Anne Hathaway, Emily Blunt, Stanley Tucci"]
+  } 
+function imprimeChamada(filmeFavorito1) { 
+    const immprimir = [imprimeChamada]
+
+   return(`Venha assistir ao filme ${filmeFavorito1.nome}, de ${filmeFavorito1.ano}, dirigido por ${filmeFavorito1.diretor} e estrelado por ${filmeFavorito1.atores}.`)
+
 }
 
-// Exercício 9
-function checaIgualdadeDesconsiderandoCase() {
-const cor1 = prompt("Digite uma cor")
-const cor2 = prompt("Digite outra cor")                
-const igual = cor1.toUpperCase() == cor2.toUpperCase()
-console.log(igual) 
+// EXERCÍCIO 14
+function criaRetangulo(lado1, lado2) {
+let resultados = {
+  largura: lado1,
+  altura: lado2,
+  perimetro: 2 * (lado1+lado2),
+  area: (lado1 * lado2)
+}
+  return(`largura: ${resultados.largura}, altura:${resultados.altura}, perimetro:${resultados.perimetro}, area:${resultados.area}`)
+} 
+
+
+// EXERCÍCIO 15
+function anonimizaPessoa() {
+  const pessoa = {
+    nome: "anonima",
+    idade: 25,
+    email: "astrodev@labenu.com.br",
+    endereco: "Rua do Futuro, 4"
+  }
+   return(`${pessoa.nome}, ${pessoa.idade}, ${pessoa.email}, ${pessoa.endereco}`)
+
+
 }
 
-// Exercício 10
-function checaRenovacaoRG() {
-const anoAtual = prompt("Digite o ano atual")
-const anonascimento = prompt("Digite ano de nascimento")
-const emissaoId = prompt("Digite emissão da carteira de identidade")
-const idade = anoAtual - anonascimento
-const tempoEmissaoId = anoAtual - emissaoId
-console.log((idade<=20&&tempoEmissaoId>=5)||(idade>20&&idade<=50&&tempoEmissaoId>=10)||(idade>50&&tempoEmissaoId>=15))
+// EXERCÍCIO 16A
+function maioresDe18(arrayDePessoas) {
+ const pessoas =  [
+    { nome: "Pedro", idade: 20 },
+    { nome: "João", idade: 10 },
+    { nome: "Paula", idade: 12 },
+    { nome: "Artur", idade: 89 } 
+  ]
+  const maioresDeDezoito = pessoas.filter((numero) => {
+   return numero.idade >= 18
+
+  
+  })
 }
 
-// Exercício 11
-function checaAnoBissexto() {
-const umAno = Number(prompt("Digite um ano"))
-const quatrocentos = (umAno%400===0)
-const quatro = (umAno%4===0)
-const cem = (umAno%100!==0)
-console.log(quatro && cem || quatrocentos)
+// EXERCÍCIO 16B
+function menoresDe18(arrayDePessoas) {
+  const pessoas =  [
+    { nome: "Pedro", idade: 20 },
+    { nome: "João", idade: 10 },
+    { nome: "Paula", idade: 12 },
+    { nome: "Artur", idade: 89 } 
+  ]
+  const menoresDeDezoito = pessoas.filter((numero) => {
+   return numero.idade < 18
+  })
 }
 
-// Exercício 12
-function checaValidadeInscricaoLabenu() {
-const idade = prompt("Você tem mais de 18 anos?")
-const ensMedio = prompt("Você possui ensino medio?")
-const hora = prompt("Você possui disponibilidade exclusiva durante os horários do curso?")
-console.log(idade =="sim" && ensMedio =="sim" && hora =="sim")
+
+// EXERCÍCIO 17A
+function multiplicaArrayPor2(array) {
+const array = [1, 2, 3, 4, 5, 6] 
+for(let i = 0; i < array.length; i ++) {
+  if(array.length * 2){
+    array.push
+  }
+} return array
+
+}
+
+// EXERCÍCIO 17B
+function multiplicaArrayPor2S(array) {
+
+}
+
+// EXERCÍCIO 17C
+function verificaParidade(array) {
+
+}
+
+// EXERCÍCIO 18A
+function retornaPessoasAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 18B
+function retornaPessoasNaoAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 19A
+function ordenaPorNome(consultasNome) {
+
+}
+
+// EXERCÍCIO 19B
+function ordenaPorData(consultasData) {
+
+}
+
+// EXERCÍCIO 20
+function calculaSaldo(contas) {
 
 }
