da793209c7d2924ade5933678011904ec8a35adf
andredsn <andresoteropolitano10@gmail.com>
2021-08-18 00:16:11 -0300

    labenusers parte 1

---
+++ Labenusers/labe-nusers/src/Components/TelaCadastro.js
@@ -0,0 +1,62 @@
+import React from "react"
+import axios from "axios"
+
+export default class TelaCadastro extends React.Component {
+ state = {
+     nome: "",
+     email: ""
+ }
+
+ handleNome = (event) => {
+     this.setState({nome: event.target.value})
+ }
+
+ handleEmail = (event) => {
+    this.setState({email: event.target.value})
+}
+
+fazerCadastro = () => {
+ const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
+ const body = {
+    name: this.state.nome,
+    email: this.state.email
+ }
+
+ axios.post(url, body, {
+     headers: {
+         Authorization: "Nathalia-Dionizio-Johnson"
+     }
+ })
+
+ .then((res) => {
+  alert("Usuário(a) cadastrado(a) com sucesso!")
+  this.setState({nome: "", email: ""})
+ })
+ .catch((err) => {
+    alert("Usuário(a) ou E-mail já cadastrado")
+ })
+
+}
+
+
+
+
+
+    render() {
+        return(
+            <div>
+                <button onClick={this.props.irParaLista}>Ir para a lista de Usuários</button>
+                <h2>Cadastro</h2>
+                <input placeholder={"Nome"}
+                value={this.state.nome}
+                onChange={this.handleNome}
+                />
+                <input placeholder={"E-mail"}
+                value={this.state.email}
+                onChange={this.handleEmail}
+                />
+                <button onClick={this.fazerCadastro}>Cadastrar</button>
+            </div>
+        )
+    }
+}
