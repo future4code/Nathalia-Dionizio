2ee7ebd01d60eedbc6fb108797d24ef4faafef7f
andredsn <andresoteropolitano10@gmail.com>
2021-08-26 11:07:10 -0300

    pendencias-comit

---
+++ semana5/labedin/build/precache-manifest.a483d93ae200eb2352b053ca5b58e1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88d046e3cd1ea440e33391d711ce94e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00adfaa675f3fc9f5574",
+    "url": "/static/css/main.a5100656.chunk.css"
+  },
+  {
+    "revision": "dd35cab1ec671b13bbed",
+    "url": "/static/js/2.65cf7749.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65cf7749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00adfaa675f3fc9f5574",
+    "url": "/static/js/main.7d812360.chunk.js"
+  },
+  {
+    "revision": "9cec4286d4e96efb4903",
+    "url": "/static/js/runtime-main.6b07746c.js"
+  }
+]);
